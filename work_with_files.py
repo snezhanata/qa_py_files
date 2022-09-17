b243c0665c54d2bcb0c0628212293e22af57629e
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-09-17 23:20:49 +0400

    init commit

--- work_with_files.py
+++ work_with_files.py
@@ -11,33 +11,43 @@ target_path = './resources/'
 source_path = './source'
 file_dir = os.listdir(source_path)
 
-with ZipFile(f'{target_path}file_4.zip', 'w', compression=ZIP_DEFLATED) as file_zip:
-    for file in file_dir:
-        add_file = os.path.join(source_path, file)
-        file_zip.write(add_file, os.path.relpath(add_file, source_path))
-    file_zip.extractall(target_path)
+def file_zip_creation(file_zip_name):
+    with ZipFile(f'{target_path}{file_zip_name}', 'w', compression=ZIP_DEFLATED) as file_zip:
+        for file in file_dir:
+            add_file = os.path.join(source_path, file)
+            file_zip.write(add_file, os.path.relpath(add_file, source_path))
+        file_zip.extractall(target_path)
 
 # Task_3: Реализовать чтение и проверку содержимого каждого файла из архива
 
-print('CSV Check:')
-with open(f'{target_path}file_3.csv') as file_csv:
-    table = list(csv.reader(file_csv))
-    for index, value in enumerate(table):
-        if index == 11:
-            print(value[4])
-    assert 'Mara' == table[2][1]
-    # for row in enumerate(table):
-    #     print(row) # распечатать весь файл
+def file_csv_check(file_csv_name):
+    print('CSV Check:')
+    with open(f'{target_path}{file_csv_name}') as file_csv:
+        table = list(csv.reader(file_csv))
+        for index, value in enumerate(table):
+            if index == 11:
+                print(value[4])
+        assert 'Mara' == table[2][1]
+        # for row in enumerate(table):
+        #     print(row) # распечатать весь файл
 
-print('\nXLSX Check:')
-workbook = load_workbook(f'{target_path}file_2.xlsx')
-sheet = workbook.active
-print(sheet.cell(row=3, column=2).value)  # OR print(sheet['B3'].value)
-assert sheet[23][1].value == 'Что нужно сделать, чтобы функция возвратила значение?'
+def file_xlsx_check(file_xlsx_name):
+    print('\nXLSX Check:')
+    workbook = load_workbook(f'{target_path}{file_xlsx_name}')
+    sheet = workbook.active
+    print(sheet.cell(row=3, column=2).value)  # OR print(sheet['B3'].value)
+    assert sheet[23][1].value == 'Что нужно сделать, чтобы функция возвратила значение?'
 
-print('\nPDF Check:')
-pdf_reader = PdfReader(f'{target_path}file_1.pdf')
-page = pdf_reader.pages[1]
-text = page.extract_text()
-print(text)
-assert 'The end, and just as well' in text
+def file_pdf_check(file_pdf_name):
+    print('\nPDF Check:')
+    pdf_reader = PdfReader(f'{target_path}{file_pdf_name}')
+    page = pdf_reader.pages[1]
+    text = page.extract_text()
+    print(text)
+    assert 'The end, and just as well' in text
+
+
+file_zip_creation('file_4.zip')
+file_csv_check('file_3.csv')
+file_xlsx_check('file_2.xlsx')
+file_pdf_check('file_1.pdf')
